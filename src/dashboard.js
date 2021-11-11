836491e5023a79eeb1ff8a8086ee6464a438c969
Arthur Cezar <arthurcvm@hotmail.com>
2021-11-10 22:59:54 -0300

    feat: implements pieChart

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,8 +1,32 @@
+import {useState} from 'react';
 import './dashboard.css';
+import Chart from 'react-google-charts';
 
-function Dashboard() {
+function Dashboard () {
+
+  const TITULO = "Quantidade de cadastros primeiro semestre"
+  
+  const [dados, setDados] = useState([
+    ['Mês', 'Quantidade'],
+    ['Janeiro', 33],
+    ['Fevereiro', 68],
+    ['Março', 49],
+    ['Abril', 15],
+    ['Maio', 80],
+    ['Junho', 27]
+  ]);
+
   return (
+    <div>
     <h1>Dashboard</h1>
+    <Chart
+      width={'400px'}
+      height={'300px'}
+      chartType={'PieChart'}
+      data={dados}
+      options={{
+        title: TITULO
+      }}/></div>
   );
 }
 
