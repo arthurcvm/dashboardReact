31f156367f7c032169ca18e3324c8c419c96774f
Arthur Cezar <arthurcvm@hotmail.com>
2021-11-12 07:52:26 -0300

    feat: implements other types of charts

--- src/dashboard.js
+++ src/dashboard.js
@@ -5,6 +5,7 @@ import Chart from 'react-google-charts';
 function Dashboard () {
 
   const TITULO = "Quantidade de cadastros primeiro semestre"
+  const ANIMACAO = {duration: 1000, easing: 'out', startup: true}
   
   const [dados, setDados] = useState([
     ['Mês', 'Quantidade'],
@@ -26,7 +27,62 @@ function Dashboard () {
       data={dados}
       options={{
         title: TITULO
-      }}/></div>
+        }} />
+      <Chart
+      width={'400px'}
+      height={'300px'}
+      chartType={'PieChart'}
+      data={dados}
+      options={{
+        title: TITULO,
+        is3D: true
+        }} />
+      <Chart
+      width={'400px'}
+      height={'300px'}
+      chartType={'PieChart'}
+      data={dados}
+      options={{
+        title: TITULO,
+        pieHole: 0.4
+        }} />
+      <Chart
+      width={'400px'}
+      height={'300px'}
+      chartType={'BarChart'}
+      data={dados}
+      options={{
+        title: TITULO,
+        chartArea: { width: '50%' },
+        hAxis: { title: 'Quantidade' },
+        vAxis: { title: 'Mês' },
+        animation: ANIMACAO
+        }} />
+      <Chart
+      width={'400px'}
+      height={'300px'}
+      chartType={'LineChart'}
+      data={dados}
+      options={{
+        title: TITULO,
+        chartArea: { width: '50%' },
+        hAxis: { title: 'Mês' },
+        vAxis: { title: 'Quantidade' },
+        animation: ANIMACAO
+        }} />
+      <Chart
+      width={'400px'}
+      height={'300px'}
+      chartType={'AreaChart'}
+      data={dados}
+      options={{
+        title: TITULO,
+        chartArea: { width: '50%' },
+        hAxis: { title: 'Mês' },
+        vAxis: { title: 'Quantidade' },
+        animation: ANIMACAO
+        }} />
+    </div>
   );
 }
 
